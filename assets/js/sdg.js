e8d4cdf88f4f2ba67cabde6296e97ab039b5404c
CircleCI <no-reply@example.com>
2019-11-19 23:25:53 +0000

    Automated deployment to GitHub Pages: 25ddcf6a0bb0732a553e4483680f5d8f1a1258c5

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1620,7 +1620,6 @@ var indicatorView = function(model, options) {
   };
 
   this.createPlot = function(chartInfo) {
-    // console.log(chartInfo);
     var that = this;
 
     var chartConfig = {
@@ -1694,7 +1693,7 @@ var indicatorView = function(model, options) {
               type: "line",
               mode: "horizontal",
               scaleID: "y-axis-0",
-              value: this._model.data[0]["Target Line"],
+              value: this._model.data[0].Value,
               borderColor: "rgb(75, 192, 192)",
               borderWidth: 4,
               label: {
