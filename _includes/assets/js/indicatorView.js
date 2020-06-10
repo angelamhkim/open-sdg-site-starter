99d600a85c65f08a502ee314a85efdf7794845ed
Linda Mejia <lindaisamarmejia@gmail.com>
2020-06-10 14:40:09 -0700

    Update indicatorView.js

--- _includes/assets/js/indicatorView.js
+++ _includes/assets/js/indicatorView.js
@@ -418,6 +418,7 @@ var indicatorView = function (model, options) {
 
     //Linear Regression Update
     chartInfo.datasets.push(linearRegression(view_obj._chartInstance.data));
+    console.log(view_obj._chartInstance.data);
     //End Linear Regression Update
 
     view_obj._chartInstance.update(1000, true);
@@ -467,6 +468,7 @@ var indicatorView = function (model, options) {
       });
 
     const my_regression = regression.linear(clean_data);
+    console.log("Linear Regression Equation:", my_regression);
 
     const useful_points = my_regression.points.map(([x, y]) => {
       return y;
@@ -477,7 +479,8 @@ var indicatorView = function (model, options) {
       data: useful_points,
       type: "line",
       borderColor: "rgb(255,99,132)",
-      bakgroundColor: "rgb(255,226,231)",
+      backgroundColor: "rgb(255,99,132)",
+      fill: false,
     };
 
     return trendsLine;
@@ -488,10 +491,10 @@ var indicatorView = function (model, options) {
   //GET NATIONAL DATA
 
   function getNationalData(chartInfo) {
-    console.log(chartInfo);
     $.get(
       `https://GSA.github.io/sdg-data-usa/data/${chartInfo.shortIndicatorId}.json`,
       function (data, status) {
+        console.log("National Data:", data);
         let years = [...new Set(data.Year)];
 
         let unique = years.concat(chartInfo.labels);
@@ -503,7 +506,6 @@ var indicatorView = function (model, options) {
         let yearIndex = unique.map((year) => {
           return data.Year.indexOf(year);
         });
-        console.log("yearsindex", yearIndex);
 
         const nationalPoints = yearIndex.map((x) => {
           return data.Value[x];
@@ -519,8 +521,16 @@ var indicatorView = function (model, options) {
       }
     );
   }
-
   //END NATIONAL DATA
+  
+  //GOAL LINE
+  function getGoal(chartInfo) {
+    var goal;
+ 
+    console.log(chartInfo);
+  }
+
+  //END GOAL LINE
 
   this.createPlot = function (chartInfo) {
     chartInfo.datasets.push(linearRegression(chartInfo));
