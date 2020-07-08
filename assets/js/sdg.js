2d6453acfa8a3ae700b24503e2c71bf87f862556
CircleCI <no-reply@example.com>
2020-07-08 16:24:00 +0000

    Automated deployment to GitHub Pages: 167191ac34cce6c84330058884bb4cc1ac8fcc32

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1685,9 +1685,11 @@ var indicatorView = function (model, options) {
       return y;
     });
 
+    const trendsPoints = Array.from(new Set(useful_points));
+
     const trendsLine = {
       label: "Trends Line",
-      data: useful_points,
+      data: trendsPoints,
       type: "line",
       borderColor: "rgb(255,99,132)",
       backgroundColor: "rgb(255,99,132)",
